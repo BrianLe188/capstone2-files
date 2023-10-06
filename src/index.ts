d9972a909d40172be8a37bf3e07463d87be5bdfd
brian.h <anhkun123456@gmail.com>
2023-10-06 19:59:56 +0700

    new: file upload queue

--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,43 @@ app.use("/upload", express.static("upload"));
 
 async function main() {
   try {
+    const { fileTypeFromBuffer } = await (eval(
+      'import("file-type")'
+    ) as Promise<typeof import("file-type")>);
+
     const amqpConnection = await amqp.connect("amqp://127.0.0.1");
     const channel = await amqpConnection.createChannel();
     const messageExchange = "file";
     const messageQueue = "upload_file";
+    const returnMessageQueue = "return_file";
 
     await channel.assertExchange(messageExchange, "direct");
     await channel.assertQueue(messageQueue);
     await channel.bindQueue(messageQueue, messageExchange, "write");
 
-    channel.consume(messageQueue, (msg) => {
-      if (msg?.fields.routingKey === "write") {
-        fs.writeFileSync(`upload/${v4}.png`, msg.content);
+    await channel.assertQueue(returnMessageQueue);
+
+    channel.consume(
+      messageQueue,
+      async (msg) => {
+        if (msg?.fields.routingKey === "write") {
+          const id = v4();
+          const buffer = msg.content;
+          const _fileType = await fileTypeFromBuffer(buffer);
+          if (_fileType) {
+            fs.writeFileSync(`upload/${id}.${_fileType?.ext}`, buffer);
+            channel.sendToQueue(
+              returnMessageQueue,
+              Buffer.from(`upload/${id}.${_fileType?.ext}`)
+            );
+          }
+          channel.ack(msg);
+        }
+      },
+      {
+        noAck: false,
       }
-    });
+    );
 
     app.listen(process.env.FILE_PORT, () => {
       console.log(`File service listening on ${process.env.FILE_PORT}`);
